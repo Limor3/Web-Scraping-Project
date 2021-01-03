5f99ec167365ee5f3b301b96071e4d0bdd34eb11
LimorNunu <63741305+LimorNunu@users.noreply.github.com>
2021-01-03 14:23:48 +0200

    Update exchange_api.py

--- exchange_api.py
+++ exchange_api.py
@@ -1,4 +1,4 @@
-from configuration import *
+import configuration as cfg
 import requests
 from datetime import date
 """
@@ -7,7 +7,7 @@ This file get price exchange rate by API.
 
 today = date.today()
 
-response = requests.get(API_REQUEST.format(today, BASE_EX_RATE, SYMBOL_EX_RATE))
+response = requests.get(cfg.API_REQUEST.format(today, cfg.BASE_EX_RATE, cfg.SYMBOL_EX_RATE))
 data = response.json()
-exchange_rate = data[GET_RATE_JASON_EX][SYMBOL_EX_RATE]
-date_exchange_rate = data[GET_DATE_JASON_EX]
+exchange_rate = data[cfg.GET_RATE_JASON_EX][cfg.SYMBOL_EX_RATE]
+date_exchange_rate = data[cfg.GET_DATE_JASON_EX]
