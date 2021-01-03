f529413db104d3f4dd480f3c3861b41d076aab96
LimorNunu <63741305+LimorNunu@users.noreply.github.com>
2021-01-03 14:28:43 +0200

    Update data_product.py

--- data_product.py
+++ data_product.py
@@ -1,6 +1,6 @@
 from html_soup import Html_soup
 from bs4 import BeautifulSoup as bs
-from configuration import *
+import configuration as cfg
 import requests
 from tqdm import tqdm
 import logging
@@ -16,7 +16,7 @@ class Data_product:
         :param class_name: string of the class name
         :return: text of the wanted class in the soup
         """
-        return soup.find(s_type, {CLASS: class_name}).text
+        return soup.find(s_type, {cfg.CLASS: class_name}).text
 
     @staticmethod
     def find_all(soup, class_name):
@@ -25,7 +25,7 @@ class Data_product:
         :param class_name: string of the class name
         :return: text of the wanted elements of the class in the soup
         """
-        return soup.find_all(DIV, {CLASS: class_name})
+        return soup.find_all(cfg.DIV, {cfg.CLASS: class_name})
 
     @staticmethod
     def get_data(url, section):
@@ -39,34 +39,34 @@ class Data_product:
         prod_dict = {}
 
         # adding ID for the product:
-        prod_dict[ID] = Data_product.soup_find(soup, DIV, CLASS_ID).strip().split()[ID_SPLIT_NUM]
+        prod_dict[ID] = Data_product.soup_find(soup, cfg.DIV, cfg.CLASS_ID).strip().split()[cfg.ID_SPLIT_NUM]
 
         # adding price for the product:
-        prod_price = float(Data_product.soup_find(soup, DIV, CLASS_PRICE).split(PRICE_SPLIT_SIGN)[PRICE_SPLIT_NUM].strip())
-        prod_dict[PRICE] = prod_price
+        prod_price = float(Data_product.soup_find(soup, cfg.DIV, cfg.CLASS_PRICE).split(cfg.PRICE_SPLIT_SIGN)[cfg.PRICE_SPLIT_NUM].strip())
+        prod_dict[cfg.PRICE] = prod_price
 
         # adding average rate for the product:
-        prod_dict[RATE] = float(Data_product.soup_find(soup, DIV, CLASS_RATE).strip())
+        prod_dict[cfg.RATE] = float(Data_product.soup_find(soup, cfg.DIV, cfg.CLASS_RATE).strip())
 
         # adding reviews amount for the product:
-        prod_dict[RVW_AMOUNT] = Data_product.soup_find(soup, SPAN, CLASS_RVW_AMOUNT).strip().split()[RVW_AMNT_SPLIT_NUM]
+        prod_dict[cfg.RVW_AMOUNT] = Data_product.soup_find(soup, cfg.SPAN, cfg.CLASS_RVW_AMOUNT).strip().split()[cfg.RVW_AMNT_SPLIT_NUM]
 
         # adding the percentage of product fit to size:
-        for sz in Data_product.find_all(soup, CLASS_FIT):
-            prod_dict[sz.text.split(FIT_SPLIT_TYPE)[FIT_FIRST_SPLIT_NUM]] = \
-                sz.text.split(FIT_SPLIT_TYPE)[FIT_SEC_SPLIT_NUM]
+        for sz in Data_product.find_all(soup, cfg.CLASS_FIT):
+            prod_dict[sz.text.split(cfg.FIT_SPLIT_TYPE)[cfg.FIT_FIRST_SPLIT_NUM]] = \
+                sz.text.split(cfg.FIT_SPLIT_TYPE)[cfg.FIT_SEC_SPLIT_NUM]
 
         # adding the description of the product:
-        for item in Data_product.find_all(soup, CLASS_DESCRIPTION):
-            prod_dict[item.text.split(DESC_SPLIT_TYPE)[DESC_FIRST_SPLIT_NUM].strip()] = \
-                item.text.split(DESC_SPLIT_TYPE)[DESC_SEC_SPLIT_NUM].strip()
+        for item in Data_product.find_all(soup, cfg.CLASS_DESCRIPTION):
+            prod_dict[item.text.split(cfg.DESC_SPLIT_TYPE)[cfg.DESC_FIRST_SPLIT_NUM].strip()] = \
+                item.text.split(cfg.DESC_SPLIT_TYPE)[cfg.DESC_SEC_SPLIT_NUM].strip()
 
         # adding the price and date from the exchange_api file
-        prod_dict[PRICE_EXCHANGE] = round(exchange_rate * prod_price, ROUND_PRICE_EX)
-        prod_dict[DATE_EXCHANGE] = date_exchange_rate
+        prod_dict[cfg.PRICE_EXCHANGE] = round(exchange_rate * prod_price, cfg.ROUND_PRICE_EX)
+        prod_dict[cfg.DATE_EXCHANGE] = date_exchange_rate
 
         # adding product_type by the section:
-        prod_dict[PRODUCT_TYPE] = section
+        prod_dict[cfg.PRODUCT_TYPE] = section
         return prod_dict
 
     @staticmethod
@@ -81,30 +81,30 @@ class Data_product:
         products_list = []      # get dict for each product
         total_url_list = []     # get all products url
 
-        for page in tqdm(range(1, NUMBER_OF_PAGES)):
+        for page in tqdm(range(1, cfg.NUMBER_OF_PAGES)):
             # changing the pages
-            url_page = url_choice + PAGE.format(page)
+            url_page = url_choice + cfg.PAGE.format(page)
 
             # create url addresses for all the dresses products in the page
-            soup = bs(requests.get(url_page).content, HTML_PRS)
-            prod = soup.findAll(URL_FIND_TYPE, href=True)
+            soup = bs(requests.get(url_page).content, cfg.HTML_PRS)
+            prod = soup.findAll(cfg.URL_FIND_TYPE, href=True)
 
             products_url_list = []
             for address in prod:
-                temp = (str(address).split(URL_SPLIT_SIGN))[TEMP_SPLIT_NUM]
-                url_prod = temp.split(URL_PROD_SPLIT_SIGN)[URL_PROD_SPLIT_NUM].strip()
-                if url_prod.endswith(HTML_END):
-                    products_url_list.append(URL + url_prod)
+                temp = (str(address).split(cfg.URL_SPLIT_SIGN))[cfg.TEMP_SPLIT_NUM]
+                url_prod = temp.split(cfg.URL_PROD_SPLIT_SIGN)[cfg.URL_PROD_SPLIT_NUM].strip()
+                if url_prod.endswith(cfg.HTML_END):
+                    products_url_list.append(cfg.URL + url_prod)
 
             # list of url for each product
-            for url1 in products_url_list[FROM: TO]:
+            for url1 in products_url_list[cfg.FROM: cfg.TO]:
                 total_url_list.append(url1)
 
         total_url_list = list(set(total_url_list))
         # the first url is the main page of all dresses, so the list start from the 1st item
 
-        logging.basicConfig(format=LOGGING_FORMAT,
-                            filename=LOGGING_FILE_NAME, filemode=LOGGING_FILE_MODE)
+        logging.basicConfig(format=cfg.LOGGING_FORMAT,
+                            filename=cfg.LOGGING_FILE_NAME, filemode=cfg.LOGGING_FILE_MODE)
         logger = logging.getLogger()
         logger.setLevel(logging.NOTSET)
 
@@ -113,10 +113,10 @@ class Data_product:
             try:
                 products_list.append(Data_product.get_data(product, section))
             except AttributeError:
-                logger.warning(LOGGING_ATT_ERROR.format(product))
+                logger.warning(cfg.LOGGING_ATT_ERROR.format(product))
                 pass
             except ValueError:
-                logger.warning(LOGGING_VAL_ERROR.format(product))
+                logger.warning(cfg.LOGGING_VAL_ERROR.format(product))
                 pass
 
         return products_list, section
