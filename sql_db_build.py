e502657b327b2109f031e188bc40f23aa0ffd1a1
LimorNunu <63741305+LimorNunu@users.noreply.github.com>
2021-01-03 13:55:51 +0200

    Update sql_db_build.py

--- sql_db_build.py
+++ sql_db_build.py
@@ -4,29 +4,29 @@ This file creates the database, and you run it only once.
 
 import pymysql.cursors
 import mysql.connector
-from configuration import *
+import configuration as cfg
 
 # Create database
-con = mysql.connector.connect(host=CONNECT_DB_HOST,
-                              user=CONNECT_DB_USER,
+con = mysql.connector.connect(host=cfg.CONNECT_DB_HOST,
+                              user=cfg.CONNECT_DB_USER,
                               password='Ab123456')
 
 cur = con.cursor()
-cur.execute(SQL_CREATE_DB)
+cur.execute(cfg.SQL_CREATE_DB)
 
 # Create tables
-con = pymysql.connect(host=CONNECT_DB_HOST,
-                      user=CONNECT_DB_USER,
+con = pymysql.connect(host=cfg.CONNECT_DB_HOST,
+                      user=cfg.CONNECT_DB_USER,
                       password='Ab123456',
-                      db=CONNECT_DB_DB,
-                      charset=CONNECT_DB_CHARSET,
+                      db=cfg.CONNECT_DB_DB,
+                      charset=cfg.CONNECT_DB_CHARSET,
                       cursorclass=pymysql.cursors.DictCursor)
 
 cur = con.cursor()
 with cur:
     cur = con.cursor()
-    cur.execute(SQL_TABLE_PRODUCTS)
-    cur.execute(SQL_TABLE_DRESSES)
-    cur.execute(SQL_TABLE_T_SHIRTS)
-    cur.execute(SQL_TABLE_SWIMWEAR)
-    cur.execute(SQL_TABLE_COMMON_DESC)
+    cur.execute(cfg.SQL_TABLE_PRODUCTS)
+    cur.execute(cfg.SQL_TABLE_DRESSES)
+    cur.execute(cfg.SQL_TABLE_T_SHIRTS)
+    cur.execute(cfg.SQL_TABLE_SWIMWEAR)
+    cur.execute(cfg.SQL_TABLE_COMMON_DESC)
